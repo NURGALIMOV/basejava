8e508bf597b200747f351fb1aaa20b8d305c22b0
Нургалимов Илшат <il.nur86@mail.ru>
2020-04-09 23:25:13 +0300

    Реализован класс ArrayStorage в рамках домашнего задания HW1.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    private int size = 0;
+
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        return Arrays.stream(storage).limit(size).filter(resume -> uuid.equals(resume.uuid)).findFirst()
+                .orElse(null);
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                for (int j = i + 1; j < size; j++, i++) {
+                    storage[i] = storage[j];
+                }
+                storage[size - 1] = null;
+                break;
+            }
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
